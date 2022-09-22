7863a5ec0fb5501a2982c403ae4b54ee8cd8311b
Quas NaArt <quas.naart@gmail.com>
2022-09-22 13:08:08 -0400

    commas now tabs to aid in import

--- leaf_blower_save_reader.py
+++ leaf_blower_save_reader.py
@@ -85,7 +85,7 @@ class LeafParser:
 
         # print(layer)
         # print(targets)
-        headerstring = ",".join([""] + targets + ["\n"])
+        headerstring = "\t".join([""] + targets + ["\n"])
         fileappend(self.out_file, headerstring.upper())
         layerstring = ".".join(layer)
         # print(layerstring)
@@ -93,8 +93,8 @@ class LeafParser:
         for item in drilled_layer:
             tempstring = ".".join([layerstring, item])
             for target in targets:
-                tempstring = ",".join([tempstring, str(drilled_layer[item][target])])
-            tempstring = ",".join([tempstring, "\n"])
+                tempstring = "\t".join([tempstring, str(drilled_layer[item][target])])
+            tempstring = "\t".join([tempstring, "\n"])
             # print(tempstring)
             fileappend(self.out_file, tempstring)
 
@@ -224,20 +224,20 @@ class LeafParser:
                 ],
             },
         }
-        fileappend(self.out_file, "STAT,VALUE,\n")
+        fileappend(self.out_file, "STAT\tVALUE\n")
         for combo in combinations:
             for stat in combinations[combo]["targets"]:
                 stat_value = self.layer_drill(combinations[combo]["layer"] + [stat])
                 fileappend(
                     self.out_file,
                     ".".join(combinations[combo]["layer"] + [stat])
-                    + ","
+                    + "\t"
                     + str(stat_value)
-                    + ",\n",
+                    + "\n",
                 )
 
     def parse_leafscends(self):
-        fileappend(self.out_file, "LEAFSCENSION,COUNT\n")
+        fileappend(self.out_file, "LEAFSCENSION\tCOUNT\n")
         layer = ["profiles", "def", "resources"]
         tempdict = self.layer_drill(layer)
         for leaf in tempdict.keys():
@@ -246,7 +246,7 @@ class LeafParser:
                     fileappend(
                         self.out_file,
                         ".".join(layer + [leaf, "leafscend", tier])
-                        + ","
+                        + "\t"
                         + str(tempdict[leaf]["leafscend_count"][tier])
                         + "\n",
                     )
@@ -275,7 +275,7 @@ class LeafParser:
             props[i] = self._multioptions[i]
 
         with open(self.out_file, "a") as file:
-            file.write("CRAFTED LEAF PROPERTY,TOTAL VALUE,\n")
+            file.write("CRAFTED LEAF PROPERTY\tTOTAL VALUE\n")
         # For each equipped leaf, read its properties and add them to the running total.
         drill_layer = ["profiles", "def", "crafted_leaves"]
         leaflist = self.layer_drill(drill_layer)
@@ -304,7 +304,7 @@ class LeafParser:
                 for j in props[i]:
                     fileappend(
                         self.out_file,
-                        ",".join(
+                        "\t".join(
                             [
                                 ".".join(drill_layer + [str(i), str(j)]),
                                 str(props[i][j]),
@@ -316,7 +316,7 @@ class LeafParser:
                 # normal outputs
                 fileappend(
                     self.out_file,
-                    ",".join([".".join(drill_layer + [str(i)]), str(props[i]), "\n"]),
+                    "\t".join([".".join(drill_layer + [str(i)]), str(props[i]), "\n"]),
                 )
 
     def parse_shops(self):
@@ -328,90 +328,15 @@ class LeafParser:
         layer = ["profiles", "def", "shops"]
         targets = ["count"]
         shop_dict = self.layer_drill(layer)
-        fileappend(self.out_file, ",".join(["SHOP.ITEM"] + targets).upper() + ",\n")
+        fileappend(self.out_file, "\t".join(["SHOP.ITEM"] + targets).upper() + "\n")
         for shop in shop_dict:
             for shopitem in shop_dict[shop]:
                 message = ".".join(layer + [shop, shopitem])
                 for target in targets:
-                    message = message + "," + str(shop_dict[shop][shopitem][target])
-                fileappend(self.out_file, message + ",\n")
+                    message = message + "\t" + str(shop_dict[shop][shopitem][target])
+                fileappend(self.out_file, message + "\n")
 
     _multioptions = dict()
-
-    # _rarities = ["common_", "uncommon_", "rare_", "epic_", "mythical_", "legendary_"]
-
-    # _enemies = [
-    #     "alb",
-    #     "angry_leaf",
-    #     "bat",
-    #     "bee",
-    #     "bird",
-    #     "bug",
-    #     "carrot",
-    #     "crab",
-    #     "elk",
-    #     "frog",
-    #     "hare",
-    #     "hyena",
-    #     "ice_leaf",
-    #     "scorpion",
-    #     "snowman",
-    #     "skull",
-    #     "vulture",
-    #     "tower_frog",
-    #     "evil_bat",
-    #     "evil_hare",
-    #     "evil_crab",
-    #     "evil_scorpion",
-    #     "evil_snowman",
-    #     "evil_brain",
-    #     "evil_bird",
-    #     "evil_carrot",
-    #     "evil_skull",
-    #     "evil_stone",
-    #     "evil_factory",
-    #     "evil_fire",
-    #     "evil_ghast",
-    #     "evil_head",
-    #     "evil_hulk",
-    #     "evil_klackon",
-    #     "evil_lurker",
-    #     "evil_mask",
-    #     "evil_skull02",
-    #     "boss_frog",
-    #     "boss_bat",
-    #     "boss_hare",
-    #     "boss_crab",
-    #     "boss_scorpion",
-    #     "boss_snowman",
-    #     "boss_brain",
-    #     "boss_dragon",
-    #     "boss_lavagolem",
-    #     "boss_bob",
-    #     "boss_stonegolem",
-    #     "boss_factory",
-    #     "boss_fire",
-    #     "boss_ghast",
-    #     "boss_head",
-    #     "boss_hulk",
-    #     "boss_lurker",
-    #     "boss_mask",
-    #     "boss_bob02",
-    #     "boss_bob03",
-    #     "boss_bob04",
-    #     "boss_bob05",
-    #     "boss_witch",
-    #     "boss_centaur",
-    #     "boss_vile_creature",
-    #     "boss_air_elemental",
-    #     "boss_spark_bubble",
-    #     "boss_terror_blue",
-    #     "boss_terror_green",
-    #     "boss_terror_red",
-    #     "boss_terror_purple",
-    #     "boss_terror_super",
-    #     "boss_energy_guard",
-    # ]
 
     _leaves = [
         "leaves",
@@ -445,59 +370,6 @@ class LeafParser:
         "tektite",
     ]
 
-    # _equipments = [
-    #     "Flail",
-    #     "cheese_boots",
-    #     "cursed_cheese",
-    #     "hammer",
-    #     "leaf_armor",
-    #     "leaf_helm",
-    #     "leaf_shield",
-    #     "lil_doggo",
-    #     "medkit",
-    #     "mini_shovel",
-    #     "ring_blc",
-    #     "ring_zoo",
-    #     "traders_suitcase",
-    #     "viking_borb",
-    #     "wizard_hat",
-    # ]
-
-    # _tools = [
-    #     "hands",
-    #     "leaf_rake",
-    #     "leaf_rake_advanced",
-    #     "leaf_blower",
-    #     "leaf_blower_advanced",
-    #     "nuclear_leaf_blower",
-    #     "airplane_turbine",
-    #     "leaf_powered_leaf_blower",
-    #     "paint_roller",
-    #     "rocket_engine",
-    #     "lazer",
-    #     "alien_turbine",
-    #     "ace_of_spades",
-    #     "blowfish",
-    #     "gfx_card",
-    #     "drill",
-    #     "fan",
-    #     "compressor",
-    #     "bellows",
-    #     "celestial_blower",
-    #     "drill_ht",
-    #     "carpenters_cup",
-    #     "holy_grail",
-    #     "bellows_ht",
-    #     "ace_of_spades_ht",
-    #     "shovel",
-    #     "shovel_ht",
-    #     "shovel_nuclear",
-    #     "trout",
-    #     "trout_ht",
-    #     "trout_nuclear",
-    #     "sword",
-    # ]
-
 
 def main():
     save_file = os.path.join(
@@ -520,39 +392,7 @@ def main():
     LP.parse_crafts()
     LP.parse_stats()
     LP.parse_shops()
-    # csv_titles = ['path', 'value']
-    # card_dict = create_dict(card_list(), save["profiles"]["def"]["cards"], 'count')
 
-    # leafscend_dict = all_leafscend_values(save)
-    # relic_dict = create_dict(relic_list(), save["profiles"]["def"]["relics"], 'count')
-
-    # equip_count_dict = create_dict(equipments, save["profiles"]["def"]["equipment"], 'count')
-    # equip_level_dict = create_dict(equipments, save["profiles"]["def"]["equipment"], 'level')
-    # unique_level_dict = create_dict(sorted(return_keys(save["profiles"]["def"]["unique_leaves"])),
-    #                                 save["profiles"]["def"]["unique_leaves"], 'level')
-    # pet_level_dict = create_dict(sorted(return_keys(save["profiles"]["def"]["pets"])),
-    #                              save["profiles"]["def"]["pets"], 'level')
-    # pet_active_dict = create_dict(sorted(return_keys(save["profiles"]["def"]["pets"])),
-    #                               save["profiles"]["def"]["pets"], 'active')
-    # tool_level_dict = create_dict(tools, save["profiles"]["def"]["shops"]["weapons"], 'level')
-    # tool_active = save["profiles"]["def"]["objects"]["o_game"]["data"]["current_weapon_key"]
-
-    # dlc_active = create_dict(return_keys(dict(list(gen_dict_extract("dlc", save))[0])),
-    #                          dict(list(gen_dict_extract("dlc", save))[0]), 'count')
-
-    # print((return_keys(save["profiles"]["def"]["crafted_leaves"])))
-    # crafted_leaves = save["profiles"]["def"]["crafted_leaves"]
-    # slap_multiplier_craft = 0
-    # for leaf in crafted_leaves:
-    #     slap_multiplier_craft += leaf['props']["slap_multiplier_craft"]
-    # print(slap_multiplier_craft)
-
-    # print(dict(list(gen_dict_extract("dlc", save))[0]))
-    # print(card_dict)
-    # with open('test.csv', 'wb') as f:
-    #     writer = csv.writer(f, delimiter=':')
-    #     for row in card_dict.items():
-    #         writer.writerow(row)
 
 if __name__ == "__main__":
     main()
